0e86828e24030f31961111a68e05ea8b2040d842
Maarten <maarten.postma1000@gmail.com>
2023-08-15 23:17:14 +0200

    Still in progress
    Adding the error handling
    Adding the form for submitting

--- script.js
+++ script.js
@@ -2,9 +2,30 @@ const url = "./data.json";
 const list = document.querySelector(".data-list");
 const existingItems = new Set(); // Keep track of existing items
 
+function sendData(url) {
+  const formEl = document.querySelector("#data-form");
+
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(formEl);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  });
+}
+
+sendData(url);
+
 // Initial call to getData
-getData();
-setInterval(getData, 1000);
+setInterval(() => {
+  getData()
+    .then((response) => {
+      // console.log("working");
+    })
+    .catch((error) => {
+      console.log("something goes wrong");
+      console.log(error);
+    });
+}, 1000);
 
 async function getData() {
   const response = await fetch(url);
